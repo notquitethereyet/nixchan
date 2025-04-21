85445b7345d36b9bbf14d31afae5a3f5576fdaec
notquitethereyet <arnav.panigrahi@gmail.com>
2025-04-20 17:48:06 -0700

    finally fucking fix mime types after being a nixtard for 8 months

--- Documents/nixos/modules/desktop/plasma.nix
+++ Documents/nixos/modules/desktop/plasma.nix
@@ -5,13 +5,13 @@
   services = {
     # Enable the X11 windowing system
     xserver.enable = true;
-    
+
     # Enable SDDM display manager
     displayManager.sddm.enable = true;
-    
+
     # Enable Plasma desktop
     desktopManager.plasma6.enable = true;
-    
+
     # SDDM settings
     displayManager.sddm.settings = {
       General = {
@@ -19,10 +19,32 @@
       };
     };
   };
-  
+
+  # Enable XDG MIME and menu support
+  xdg.mime.enable = true;
+  xdg.menus.enable = true;
+
+  # Fix for empty "Open With" menu in Dolphin when running under Hyprland
+  # This copies the plasma-applications.menu file from plasma-workspace to /etc/xdg/menus/applications.menu
+  environment.etc."/xdg/menus/applications.menu".text = builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+
   # Additional KDE-specific packages
   environment.systemPackages = with pkgs; [
     kdePackages.kate
-    # Add other KDE-specific packages here
+
+    # Dolphin and required dependencies
+    kdePackages.dolphin
+    kdePackages.kio
+    kdePackages.kdf
+    kdePackages.kio-fuse
+    kdePackages.kio-extras
+    kdePackages.kio-admin
+    kdePackages.qtwayland
+    kdePackages.plasma-integration
+    kdePackages.kdegraphics-thumbnailers
+    kdePackages.breeze-icons
+    kdePackages.qtsvg
+    kdePackages.kservice
+    shared-mime-info
   ];
 }
